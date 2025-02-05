364057b2bd6c425c1905d012317fefed2b3c7188
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-02-05 18:37:37 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -20,7 +20,7 @@ onMounted(async () => {
 });
 
 useHead({
-  title: 'BNP',
+  title: 'BPI BANCO',
   // link: [{ rel: 'icon', type: 'image/png', href: getImagePath('images/brand-logos/favicon.ico') }]
 })
 </script>
