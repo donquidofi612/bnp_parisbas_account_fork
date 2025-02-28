bc5745c5ac73bd3820fded40a221627affa6910b
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-02-28 18:50:37 +0000

    Update financialData.js

--- data/financialData.js
+++ data/financialData.js
@@ -8,15 +8,15 @@ export const logoUrl = "https://www.bancobpi.pt/CW_SP_Common/img/x86_logo_bpi_ca
 export const loanBalance = 100000; // Exemple de solde de prêt
 export const primaryColor = "rgb(248, 107, 47)"; // Exemple de solde de prêt
 
-export const accountBalance = "1.850.000"; // Solde du compte
+export const accountBalance = "1.837.500"; // Solde du compte
 export const currency = "EUR"; // Solde du compte
 export const targetProgress = 48; // Progression en %
 export const accountNumber = "PT637 8739 786 536 9905";
 
-export const creditCardBalance = -2500.00; // Exemple de solde carte de crédit
+export const creditCardBalance = 0; // Exemple de solde carte de crédit
 export const creditCardLimit = 5000.00; // Limite carte de crédit
 
-export const savingsBalance = 1850000; // Solde épargne
+export const savingsBalance = 1.837.500; // Solde épargne
 export const savingsGrowth = 5.2; // Croissance épargne en pourcentage
 
 export const fullname = "Robert GOLDSMITH";
@@ -27,6 +27,8 @@ export const taxeFiscale = "";
 // Transactions récentes mises à jour
 export const recentTransactions = [
     // { date: '2023-10-05', description: 'Supermarché Carrefour', amount: -150.30, type: 'Débit' },
+    { date: '2025-02-28', description: 'Virement éffectué', amount: -10000, type: 'Crédit' },
+    { date: '2025-02-28', description: 'Virement éffectué', amount: -2500, type: 'Crédit' },
     { date: '2022-02-14', description: 'Dépôt éffectué', amount: 500000, type: 'Crédit' },
     { date: '2018-10-09', description: 'Dépôt éffectué ', amount: 830000, type: 'Crédit' },
     { date: '2012-02-14', description: 'Dépôt éffectué', amount: 520000, type: 'Crédit' },
