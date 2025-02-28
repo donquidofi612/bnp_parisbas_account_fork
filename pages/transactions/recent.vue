b15b72b17ca559f8bc0e00c987b979cff0f336b8
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-02-28 19:41:31 +0000

    Update recent.vue

--- pages/transactions/recent.vue
+++ pages/transactions/recent.vue
@@ -148,7 +148,7 @@ export default {
         "description": "Dépôt éffectué",
         "type": "Crédit",
         "amount": 520000,
-        "balanceAfter": 1.837.500
+        "balanceAfter": 1837500
       }
     ]);
 
